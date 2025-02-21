53d415129c1c94135db9a366ea0f54e5b2f42088
anant5441 <anantji2332@gmail.com>
2025-02-21 22:57:52 +0530

    Add file

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+include("./header.php");
+?>
+    <div class="welcome-area">
+        <div class="owl-carousel slider-content">
+            <div class="single-slider-item slider-bg-1">
+                <div class="slider-inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="slide-text">
+                                    <h2>We provide quality education</h2>
+                                    <p>Education is what remains after one has forgotten<br>
+                                        what one has learned in school.
+
+                                        <br><i>Albert Einstein</i>
+                                    </p>
+
+                                    <a href="" class="boxed-btn">Learn more <i class="fa fa-long-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider-item slider-bg-2">
+                <div class="slider-inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="slide-text">
+                                    <h2>We provide intensive care</h2>
+                                    <p>Education is the most powerful weapon<br>
+                                        which you can use to change the world.
+
+                                        <br><i>Nelson Mandela</i>
+                                    </p>
+
+                                    <a href="" class="boxed-btn">Learn more <i class="fa fa-long-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--=========================== Content-area ============================-->
+    <div class="content-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <?php include('component/controller.php'); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--========================== Footer-area ===============================-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="widget">
+                        <div class="logo">
+                            <a href="">
+                                <i class="fa fa-home"></i>
+                                <span>School</span>
+                            </a>
+                            <p> Education is what remains after one has forgotten <br>
+                                what one has learned in school.
+
+                                <br><i>Albert Einstein</i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="widget">
+                        <h3>Navigation</h3>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="?a=student_add">Admission</a></li>
+                                <li><a href="?a=view">Students</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <h3>Contact Us</h3>
+                    <span class="social-icon">
+                        <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://google.com" target="_blank"><i class="fab fa-google"></i></a>
+                    </span>
+                </div>
+                <p class="copy-right">Copyright &copy;Anant</p>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="assets/js/popper-1.12.9.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
