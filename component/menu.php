a73ffd4b60ee7f101d6bebf2920d79733fe9083e
anant5441 <anantji2332@gmail.com>
2025-02-22 23:43:05 +0530

    Add file

---
+++ component/menu.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="?a=student_add">Admission</a></li>
+    <li><a href="?a=view">Students</a></li>
+    <li><a href="?a=contact">Contact Us</a></li>
+</ul>
